4e2dcf18537bcc533c22938649f2115e95787c0f
topherbuckley <topherbuckley@gmail.com>
2017-06-27 21:46:08 +0900

    Fixing relative references again

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /**** Photo-Sphere-Viewer ****/
 viewer = new PhotoSphereViewer({
   container: 'photosphere',
-  panorama: '/images/beach.png',
+  panorama: 'https://raw.githubusercontent.com/topherbuckley/omnitone-PSV/master/images/beach.png',
   navbar: [
     'zoom',
     'caption',
@@ -13,7 +13,7 @@ viewer = new PhotoSphereViewer({
 /**** Omnitone Code ****/
 // Set up an audio element to feed the ambisonic source audio feed.
 var audioElement = document.createElement('audio');
-audioElement.src = '/audio/AmbisonicSeasideFull.wav';
+audioElement.src = 'https://raw.githubusercontent.com/topherbuckley/omnitone-PSV/master/audio/AmbisonicSeasideFull.wav';
 audioElement.loop = true;
 
 // Create AudioContext, MediaElementSourceNode and FOARenderer.
@@ -21,7 +21,7 @@ var audioContext = new AudioContext();
 var audioElementSource =
     audioContext.createMediaElementSource(audioElement);
 var foaRenderer = Omnitone.createFOARenderer(audioContext, {
-    HRIRUrl: '/HRTFs/sh_hrir_o_1.wav'
+    HRIRUrl: 'https://raw.githubusercontent.com/topherbuckley/omnitone-PSV/master/HRTFs/sh_hrir_o_1.wav'
   });
 
 // Make connection and start play.
